3fd103af3852dc842998adc2bd6f34b9a5a58514
Merculov Eva <emerculov2@gmail.com>
2025-04-30 11:33:50 +0300

    Redirection to /user endpoint after authentification

--- back-end/src/main/java/com/review/reviewservice/config/SecurityConfig.java
+++ back-end/src/main/java/com/review/reviewservice/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -26,25 +28,29 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
 
-                .authorizeHttpRequests(authorizeRequests ->  authorizeRequests
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/feedbacks/**").authenticated()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
                 )
 
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/oauth2/authorization/bitbucket") // pagina de login
-                        .defaultSuccessUrl("/", true) // unde ajunge user-ul după login
+                .oauth2Login(oauth -> oauth
+                        .loginPage("/oauth2/authorization/bitbucket")
+                        .successHandler(authenticationSuccessHandler()) // redirecționare custom
                         .userInfoEndpoint(userInfo -> userInfo
                                 .userService(customOAuth2UserService)
                         )
                 )
 
-
                 .logout(logout -> logout
-                        .logoutSuccessUrl("/") // unde ajunge user-ul la logout
+                        .logoutSuccessUrl("http://localhost:3000") // redirect la frontend
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new SimpleUrlAuthenticationSuccessHandler("http://localhost:3000/user");
     }
 }
