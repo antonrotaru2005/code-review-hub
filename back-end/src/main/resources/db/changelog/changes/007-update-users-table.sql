79f1bc9c70d69e2416decda5bbeeccfbacad5a86
antonrotaru <antonrotaru3@gmail.com>
2025-05-04 12:42:16 +0300

    Implemented possibility of choosing the AI and its model

---
+++ back-end/src/main/resources/db/changelog/changes/007-update-users-table.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+
+-- changeset antonrotaru:007-update-users-table
+ALTER TABLE users
+    ADD COLUMN ai_model_id BIGINT,
+    ADD CONSTRAINT fk_ai_model
+        FOREIGN KEY (ai_model_id)
+            REFERENCES ai_models(id)
+            ON DELETE SET NULL;
