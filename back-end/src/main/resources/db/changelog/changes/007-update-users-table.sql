889e741b744df92c02d1af1346acc33715cbaafc
antonrotaru <antonrotaru3@gmail.com>
2025-05-05 15:56:04 +0300

    Updated table users

--- back-end/src/main/resources/db/changelog/changes/007-update-users-table.sql
+++ back-end/src/main/resources/db/changelog/changes/007-update-users-table.sql
@@ -2,7 +2,7 @@
 
 -- changeset antonrotaru:007-update-users-table
 ALTER TABLE users
-    ADD COLUMN ai_model_id BIGINT,
+    ADD COLUMN ai_model_id BIGINT DEFAULT 1,
     ADD CONSTRAINT fk_ai_model
         FOREIGN KEY (ai_model_id)
             REFERENCES ai_models(id)
